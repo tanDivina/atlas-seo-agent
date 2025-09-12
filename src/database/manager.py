f8f07f6920180c3eeb8fce18fdfab9d9824c2c0b
Dorien Van den Abbeele <dorienvandenabbeele@Doriens-MacBook-Air.local>
2025-09-12 17:15:49 -0500

    Add pool_pre_ping=True to engine to handle stale connections

--- src/database/manager.py
+++ src/database/manager.py
@@ -10,7 +10,7 @@ dotenv_path = os.path.join(os.path.dirname(__file__), '..', '..', '.env')
 load_dotenv(dotenv_path=dotenv_path)
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
